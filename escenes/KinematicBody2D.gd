ae8eb4bdd6aee453788d8c2e948c1df8eedc66f6
aileenlap <101245773+aileenlap@users.noreply.github.com>
2022-03-18 12:25:54 +0100

    salts, doble salts, imatges jugador

--- escenes/KinematicBody2D.gd
+++ escenes/KinematicBody2D.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 
 var velocitat_base = 200
 var velocitat = Vector2.ZERO
-var acceleracio = Vector2.DOWN * 98
+var acceleracio = Vector2.DOWN * 980
+var salt = Vector2.UP * 400
+var salts_disp = 1
 
 func _physics_process(delta):
 	
@@ -13,6 +15,14 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("mou adalt") and salts_disp > 0:
+		velocitat.y = salt.y
+		salts_disp -= 1
+	if is_on_floor():
+		salts_disp = 1
 	
 	velocitat += acceleracio * delta
-	velocitat = move_and_slide(velocitat)
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	
+#	if positon.y < -600:
+#		position = Vector2(301, 387)
