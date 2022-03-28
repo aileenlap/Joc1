9b5962ad967563dad9c3738779484ed48204c9cf
aileenlap <101245773+aileenlap@users.noreply.github.com>
2022-03-28 12:16:12 +0200

    animació hit, canvi d'escena, joc acabat

--- escenes/KinematicBody2D.gd
+++ escenes/KinematicBody2D.gd
@@ -6,6 +6,7 @@ var velocitat = Vector2.ZERO
 var acceleracio = Vector2.DOWN * 980
 var salt = Vector2.UP * 500
 var salts_disp = 1
+var hit = false
 
 func _physics_process(delta):
 	
@@ -33,21 +34,22 @@ func _physics_process(delta):
 	
 func anima(velocitat: Vector2):
 	var animacio : AnimatedSprite = $AnimatedSprite
-	if velocitat.x > 0:
-		animacio.flip_h = false
-		animacio.play('camina')
-	elif velocitat.x < 0:
-		animacio.flip_h = true
-		animacio.play('camina')
-	
-	if abs(velocitat.x) < 0.1:
-		animacio.play('quiet')
+	if not hit:
+		if velocitat.x > 0:
+			animacio.flip_h = false
+			animacio.play('camina')
+		elif velocitat.x < 0:
+			animacio.flip_h = true
+			animacio.play('camina')
 		
-	if velocitat.y < -1:
-		animacio.play('salta')
-	
-	if Input.is_action_pressed("mou abaix") and is_on_floor():
-		animacio.play('agacha')
+		if abs(velocitat.x) < 0.1:
+			animacio.play('quiet')
+			
+		if velocitat.y < -1:
+			animacio.play('salta')
+		
+		if Input.is_action_pressed("mou abaix") and is_on_floor():
+			animacio.play('agacha')
 		
 		
 		
@@ -61,8 +63,15 @@ func _on_Porta_body_entered(body):
 func _on_Enemic_body_entered(body):
 	var animacio : AnimatedSprite = $AnimatedSprite
 	animacio.play('hit')
-	position = Vector2(201, 399)
+	hit = true
+	
 	
 func _on_Port_body_entered(body):
-	get_tree().change_scene("res://escenes/Fons1.tscn")
+	get_tree().change_scene("res://escenes/Fons3.tscn")
 	position = Vector2(439, -232)
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == 'hit':
+		hit = false
+		position = Vector2(201, 399)
